1a86e33ad157866db3c7cbc8ff4f457caa791ac0
cfue <curtis.ch.chat@gmail.com>
2020-06-13 16:57:43 -0600

    add my fist square

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log("hi there we gonna crush this");
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
 const engine = Engine.create();
@@ -14,7 +13,13 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-  isStatic: true,
-});
-World.add(world, shape);
+// Walls
+const walls = [
+  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
+  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
+  Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
+];
+World.add(world, walls);
+
+World.add(world, Bodies.rectangle(200, 200, 50, 50));
