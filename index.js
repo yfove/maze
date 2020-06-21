8bac02eecb621e0c26deacf77ea8d0644db409b4
cfue <curtis.ch.chat@gmail.com>
2020-06-21 16:46:19 -0600

    keypresses added

--- index.js
+++ index.js
@@ -166,3 +166,21 @@ World.add(world, goal);
 // Ball
 const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4);
 World.add(world, ball);
+
+document.addEventListener("keydown", (event) => {
+  if (event.keyCode === 87) {
+    console.log("move ball up");
+  }
+
+  if (event.keyCode === 68) {
+    console.log("move ball right");
+  }
+
+  if (event.keyCode === 83) {
+    console.log("move ball down");
+  }
+
+  if (event.keyCode === 65) {
+    console.log("move ball left");
+  }
+});
